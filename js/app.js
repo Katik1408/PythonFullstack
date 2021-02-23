47d05f6d0452c364538ee34f46ddf935f2eff10f
Karthik Saxena <katik.ks@gmail.com>
2021-02-23 08:21:09 +0530

    changes

---
+++ js/app.js
@@ -0,0 +1,96 @@
+// // console.log('Hello from File');
+
+// //alert('Alert from file');
+
+// //let isAgree = confirm('DO you agree');
+// //console.log(isAgree);
+// // let uname = prompt();
+// // console.log(uname);
+
+// // //Variables
+// // let a = 5;
+// // var b = 6;
+// // const c = 3.14;
+
+// // a = 7;
+// // b = 8.16;
+
+// // console.log(window.a);
+// // console.log(window.b);
+// // //console.log(c);
+
+// let firstName = "Rahul"; // Type String
+// let lastName; // Absence of Type
+// let age = 22; // Type Number
+// let isActiveUser = true; //Type Boolean
+// let dateofjoining = null; //Absence of any Value
+
+// setTimeout(() => {
+//   console.log(firstName);
+// }, 5000);
+
+// //console.log(firstName);
+
+// // console.log(typeof firstName);
+// // console.log(typeof lastName);
+// // console.log(typeof age);
+// // console.log(typeof isActiveUser);
+// // console.log(typeof dateofjoining);
+
+// //Conditional Statements
+
+
+let b = "7";
+
+// if (a > b) {
+//   console.log("A is greater");
+// } else if (b > a) {
+//   console.log("B is greater");
+// } else {
+//   console.log("A and B both are same");
+// }
+
+// if (a == b) {
+//   console.log("A and B are same");
+// } else {
+//   console.log("Not same");
+// }
+
+
+// if (a === b) {
+//   console.log("A and B are same");
+// } else {
+//   console.log("Not same");
+// }
+let a = 4;
+switch(a){
+  case 1:
+    console.log('Value of a is '+ a);
+    break;
+  case 2:
+    console.log('Value of a is '+ a);
+    break;
+  case 3:
+    console.log('Value of a is '+a)
+    break;
+  default:
+    console.log('Value of a is not 1 2 and 3  Value is ' + a);
+}
+
+
+/**
+ *
+ * > Greater than
+ * < Less than
+ * >= Greater than or equal to
+ * <= Less than or equal to
+ * == double equal to
+ * === tripple equal to
+ * && AND
+ * || OR
+ * ! NOT
+ *
+ *
+ *
+ *
+ */
